4db20a6102c6d097caa7e5f1e41b1412cd494648
jcsalasdev <jcsalasdev@gmail.com>
2021-10-03 23:04:25 +0800

    updated

--- app/models/manga.rb
+++ app/models/manga.rb
@@ -1,4 +1,4 @@
 class Manga < ApplicationRecord
-    validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+    validates :title, presence: true, length: { minimum: 4, maximum: 100 }
     validates :summary, presence: true, length: { minimum: 15, maximum: 350 }
 end
